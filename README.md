316e324aad4e4d569624c2c1b2cb51becac05546
boxdox <vaibhavkandwal@pm.me>
2020-10-03 23:27:55 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# fetch my configs into current working directory
+
+### run `npx @boxdox/config`
---
+++ index.js
@@ -0,0 +1,63 @@
+const { writeFileSync } = require("fs")
+const inquirer = require("inquirer")
+const fetch = require("node-fetch")
+const chalk = require("chalk")
+
+const fetcher = fileName =>
+  fetch(
+    `https://raw.githubusercontent.com/boxdox/configs/main/files/${fileName}`
+  )
+    .then(res => res.text())
+    .catch(e => errorMsg(e))
+
+console.log(chalk.green("configs by @boxdox"))
+console.log(chalk.green("---------------------"))
+
+inquirer
+  .prompt([
+    {
+      type: "checkbox",
+      message: "Choose Configs:",
+      name: "configList",
+      choices: [
+        {
+          name: ".editorconfig",
+          checked: true,
+        },
+        {
+          name: ".prettierrc",
+          checked: true,
+        },
+      ],
+    },
+    {
+      type: "confirm",
+      message: "Using Typescript?",
+      name: "typescript",
+      default: true,
+    },
+  ])
+  .then(selected => {
+    const { configList, typescript } = selected
+
+    configList.forEach(config => {
+      fetcher(config)
+        .then(content => {
+          writeFileSync(config, content, (err, _) => {
+            if (err) return console.log(chalk.red(err))
+          })
+          console.log(chalk.blue(`wrote ${config}`))
+        })
+        .catch(err => console.log(chalk.red(err)))
+    })
+
+    if (typescript)
+      fetcher("tsconfig.json")
+        .then(content => {
+          writeFileSync("tsconfig.json", content, (err, _) => {
+            if (err) return console.log(chalk.red(err))
+          })
+          console.log(chalk.blue(`wrote tsconfig.json`))
+        })
+        .catch(err => console.log(chalk.red(err)))
+  })
